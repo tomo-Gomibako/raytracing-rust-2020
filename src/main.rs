5b2291e7b46ca84ae29bbc2d5e1a143ce42ba0d8
tomon9086 <tomo.mouse.9086@gmail.com>
2020-04-09 15:50:45 +0900

    変数名imageでも大丈夫そう

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use color::Color;
 
 fn main() {
   let size = (200, 100);
-  let mut pic = Image::new(size.0, size.1);
+  let mut image = Image::new(size.0, size.1);
 
   for x in 0..size.0 {
     for y in 0..size.1 {
       let r = (255 * x / size.0) as u8;
       let g = (255 * y / size.1) as u8;
       let b = 255 / 2;
-      pic.set_color(x, y, Color(r, g, b));
+      image.set_color(x, y, Color(r, g, b));
     }
   }
-  
-  let _ = pic.output();
+
+  let _ = image.output();
 }
